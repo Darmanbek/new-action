5e7a986d8298549aed2edc754916a13b265612eb
Darmanbek <pahratdinovdarman@gmail.com>
2024-08-27 16:23:21 +0500

    Update Project 23.0

--- src/config/pusher.config.ts
+++ src/config/pusher.config.ts
@@ -1,8 +1,8 @@
-export const PUSHER_APP_ID = "1849335";
+export const PUSHER_APP_ID = "1855937";
 // export const PUSHER_KEY = "c59e45bb8a55c6c5f01e";
-export const PUSHER_KEY = "3eee81e9fa996a2c3073";
+export const PUSHER_KEY = "e574d9498eb20491308d";
 // export const PUSHER_SECRET = "69be62c3a6b339f22cfc";
-export const PUSHER_SECRET = "09d8e2ce4eadcc4f4a59";
+export const PUSHER_SECRET = "76e98072e74b6d88037a";
 export const PUSHER_CLUSTER = "ap2";
 
 export const pusherOptions = {
