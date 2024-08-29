62c5a89677b747dfdf1f59e1719cb0b940c6a8d7
Darmanbek <pahratdinovdarman@gmail.com>
2024-08-29 17:29:51 +0500

    Update Project 24.4

--- src/components/shared/Assessments/AssessmentsValue.tsx
+++ src/components/shared/Assessments/AssessmentsValue.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-import { ApproveCheckValue } from "src/components/shared";
 import { TAssessment } from "src/services/index.types";
 
 interface AssessmentsValueProps {
@@ -11,16 +10,7 @@ const AssessmentsValue: FC<AssessmentsValueProps> = ({ date, assessments }) => {
 	const currentAssessments = assessments.find(el => el.date === date);
 	if (!currentAssessments) return "";
 
-	if (currentAssessments.is_available) return currentAssessments.value;
-
-	return (
-		<ApproveCheckValue
-			colorInverse={true}
-			isValue={currentAssessments.is_available}
-			yesText={"Был"}
-			noText={currentAssessments.consented ? "Нет с причиной" : "Нет"}
-		/>
-	);
+	return currentAssessments.value;
 };
 
 export { AssessmentsValue };
