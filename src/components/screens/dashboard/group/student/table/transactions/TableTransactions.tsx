33fc92807597fe667b235a3ea7de5f023429e5db
Darmanbek <pahratdinovdarman@gmail.com>
2024-08-29 18:40:29 +0500

    Update Project 24.6

--- src/components/screens/dashboard/group/student/table/transactions/TableTransactions.tsx
+++ src/components/screens/dashboard/group/student/table/transactions/TableTransactions.tsx
@@ -1,13 +1,19 @@
-import { FC } from "react";
+import { CalendarOutlined } from "@ant-design/icons";
+import { Space } from "antd";
+import { RangePickerProps } from "antd/es/date-picker";
+import dayjs from "dayjs";
+import { FC, useState } from "react";
 import { useParams } from "react-router-dom";
 import { HeadTable } from "src/components/shared";
 import { TTransaction } from "src/services/index.types";
 import { useColumnsTransactions } from "./useColumnsTransactions";
-import { UiTable } from "src/components/ui";
+import { UiRangePicker, UiTable, UiTooltipButton } from "src/components/ui";
 import { useGetDashboardCompaniesGroupsByIdQuery } from "src/services/index.api";
 
 const TableTransactions: FC = () => {
 	const { group_id, student_id } = useParams();
+
+	const [date, setDate] = useState<RangePickerProps["value"]>();
 
 	const {
 		data: group,
@@ -23,11 +29,34 @@ const TableTransactions: FC = () => {
 			title={() => (
 				<HeadTable
 					title={"Транзакции"}
+					children={[
+						<Space.Compact key={"Range Date"}>
+							<UiRangePicker
+								value={date}
+								onChange={setDate}
+							/>
+							<UiTooltipButton
+								type={"primary"}
+								title={"Текущий месяц"}
+								icon={<CalendarOutlined />}
+								onClick={() => setDate([dayjs().startOf("month"), dayjs().endOf("month")])}
+							/>
+						</Space.Compact>,
+					]}
 				/>
 			)}
 			dataSource={
-				group?.data.students?.find((el) => el.id === student_id)
+				group
+					?.data
+					?.students
+					?.find((el) => el.id === student_id)
 					?.transactions
+					?.filter(el => {
+						if (!date) return true;
+						const [start, end] = date;
+						if (!start && !end) return true;
+						return dayjs(el.date).isBetween(start, end);
+					})
 			}
 			columns={columns}
 			loading={isLoading || isFetching}
--- src/components/screens/group/student/table/transactions/TableTransactions.tsx
+++ src/components/screens/group/student/table/transactions/TableTransactions.tsx
@@ -1,15 +1,23 @@
-import { PlusOutlined } from "@ant-design/icons";
-import { FC } from "react";
+import { CalendarOutlined, PlusOutlined } from "@ant-design/icons";
+import { Space } from "antd";
+import { RangePickerProps } from "antd/es/date-picker";
+import dayjs from "dayjs";
+import isBetween from "dayjs/plugin/isBetween";
+import { FC, useState } from "react";
 import { useParams } from "react-router-dom";
 import { HeadTable } from "src/components/shared";
 import { TTransaction } from "src/services/index.types";
 import { useFormStorageStore } from "src/store";
 import { useColumnsTransactions } from "./useColumnsTransactions";
-import { UiTable, UiTooltipButton } from "src/components/ui";
+import { UiRangePicker, UiTable, UiTooltipButton } from "src/components/ui";
 import { useGetGroupsByIdStudentsQuery } from "src/services/index.api";
+
+dayjs.extend(isBetween);
 
 const TableTransactions: FC = () => {
 	const { group_id, student_id } = useParams();
+
+	const [date, setDate] = useState<RangePickerProps["value"]>();
 
 	const columns = useColumnsTransactions();
 
@@ -24,6 +32,18 @@ const TableTransactions: FC = () => {
 				<HeadTable
 					title={"Транзакции"}
 					children={[
+						<Space.Compact key={"Range Date"}>
+							<UiRangePicker
+								value={date}
+								onChange={setDate}
+							/>
+							<UiTooltipButton
+								type={"primary"}
+								title={"Текущий месяц"}
+								icon={<CalendarOutlined />}
+								onClick={() => setDate([dayjs().startOf("month"), dayjs().endOf("month")])}
+							/>
+						</Space.Compact>,
 						<UiTooltipButton
 							title="Добавить"
 							key="Add_Button"
@@ -37,8 +57,16 @@ const TableTransactions: FC = () => {
 				/>
 			)}
 			dataSource={
-				students?.data?.find((el) => el.id === student_id)
+				students
+					?.data
+					?.find((el) => el.id === student_id)
 					?.transactions
+					?.filter(el => {
+						if (!date) return true;
+						const [start, end] = date;
+						if (!start && !end) return true;
+						return dayjs(el.date).isBetween(start, end);
+					})
 			}
 			columns={columns}
 			loading={isLoading || isFetching}
